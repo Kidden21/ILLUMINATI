43910cdeb425c9414e71bff4cd79537ef37a4fd3
kidden <wjchi7@student.monash.edu>
2018-08-28 11:02:29 +0800

    Updates

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,41 +1,57 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+exports.createNewData = functions.firestore.document('zzchecking/{stationName}').onUpdate((change, context) => {
+	const newValue = change.after.data();
+	const waterLevel = newValue.Info.Water_Level;
+	const alertLevel = newValue.Alert_System.Alert_Level;
+	const warningLevel = newValue.Alert_System.Warning_Level;
+	const dangerLevel = newValue.Alert_System.Danger_Level;
+	const message = "The current water level is in DANGER level with a water level of ";
+	
+	if (waterLevel >= dangerLevel) {
+		console.log(message + waterLevel);
+	} else if (waterLevel >= warningLevel) {
+		console.log(message + waterLevel);
+	} else if (waterLevel >= alertLevel){
+		console.log(message + waterLevel);
+	}
+	return message + waterLevel;
+});
+
+// exports.addMessage = functions.https.onRequest(async (req, res) =>{
+// 	const original = req.query.text;
+// 	const writeResult = await admin.firestore().collection('BaganDatuk/2010-1').add({original: original});
+// 	res.json({result: 'Message with ID: ${writeResult.id} added.'});
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	console.log("hello")
-	response.send("Hello from Kidden!");
-});
+	// return admin.database().ref('/messages').push({original: original}).then((snapshot)=>{
+	// 	return res.redirect(303, snapshot.ref.toString());
+	// 	});
+// });
+//
+// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, context) =>{
+// 	const original = snapshot.val();
+// 	console.log('Uppercasing', context.params.pushId, original);
+// 	const uppercase = original.toUpperCase();
+// 	return snapshot.ref.parent.child('uppercase').set(uppercase);
+// });
 
-exports.findItem = functions.https.onRequest((request, response) => {
-	const serviceAccount = require("/Users/Kidden/Desktop/fit5120-ddc5582972f2.json");
-	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount)
-	});
-	const db = admin.firestore();
-	const doc_ref = db.collection("BaganDatuk").doc("2011-2");
-	const get_doc = doc_ref.get().then(doc => {
-		if(!doc.exists){
-			console.log("No such document!");
-		} else {
-			console.log("Document Data:", doc.data());
-		}
-	});	
-});
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+// 	console.log("hello")
+// 	response.send("Hello from Kidden!");
+// });
 
 
 // var serviceAccount = require('/Users/Kidden/Desktop/fit5120-ddc5582972f2.json');
-//
 // admin.initializeApp({
 // 	credential: admin.credential.cert(serviceAccount)
 // });
-//
 // var db = admin.firestore();
-//
 // var doc_ref = db.collection("BaganDatuk").doc("2011-2");
-//
 // var get_doc = doc_ref.get().then(doc => {
 // 	if (!doc.exists){
 // 		console.log('no such document!');
@@ -44,16 +60,5 @@ exports.findItem = functions.https.onRequest((request, response) => {
 // 	}
 // })
 
-// exports.addMessage = functions.https.onRequest((req, res) =>{
-// 	const original = req.query.text;
-// 	return admin.database().ref('/messages').push({original: original}).then((snapshot)=>{
-// 		return res.redirect(303, snapshot.ref.toString());
-// 	});
-// });
-//
-// exports.makeUppercase - functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, context) =>{
-// 	const original = snapshot.val();
-// 	console.log('Uppercasing', context.params.pushId, original);
-// 	const uppercase = original.toUpperCase();
-// 	return snapshot.ref.parent.child('uppercase').set(uppercase);
-// });
+
+
